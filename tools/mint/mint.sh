d8a07942b7457333b52a9df56ee871d62b483c08
Stewart Thompson <stewartthompson12@gmail.com>
2026-02-24 12:04:42 -0700

    Update mintlify as well

--- tools/mint/mint.sh
+++ tools/mint/mint.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 # Change to the redo directory where docs.json is located
 cd "$BUILD_WORKSPACE_DIRECTORY/redo"
 
-# Run mint with all provided arguments
+# Run mintlify with all provided arguments
 # If no arguments provided, default to "dev"
 if [ $# -eq 0 ]; then
-    mint dev
+    mintlify dev
 else
-    mint "$@"
+    mintlify "$@"
 fi
